35a4164f0fdd4650df3ef0b1cffa477e282357c8
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-02-19 18:51:23 +0200

    NcuN

---
+++ ExercisesCsharp/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExercisesCsharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            NcuN(3);
+        }
+        public static void NcuN(int n)
+        {
+            for (int i = 0; i < n; i++)
+            {
+                for (int i = 0; i < n-1; i++)
+                {
+                    Console.Write("+");
+                }
+                Console.WriteLine("+");
+            }
+        }
+    }
+}
