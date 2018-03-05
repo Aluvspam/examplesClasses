3d27c0bb9cb98d6738e41e08fa015939561307a7
andrei patrascu <andreipatrascu@outlook.com>
2018-03-05 19:04:13 +0200

    commit local added new problem with catYears dogYears

---
+++ ExercisesCsharp/Dinglemouse.cs
@@ -0,0 +1,21 @@
+﻿namespace ExercisesCsharp
+{
+    public class Dinglemouse
+    {
+        public static int[] humanYearsCatYearsDogYears(int humanYears) //humanYears >= 1
+        {
+            // Your code here!
+            var catYears = 0;
+            var dogYears = 0;
+            return new int[] { humanYears, catYears, dogYears };
+        }
+    }
+}/*
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that*/
