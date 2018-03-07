96bc7a4d10448c2adc72295755eead910e0b70e6
andrei patrascu <andreipatrascu@outlook.com>
2018-03-07 19:50:49 +0200

    tried to separate Andrei's and Oana's sollutions for cat/dog Years problem

--- ExercisesCsharp/DingleMouse.cs
+++ ExercisesCsharp/DingleMouse.cs
@@ -1,79 +1,75 @@
-﻿using System;
-
-namespace DingleMouse
+﻿namespace ExercisesCsharp
 {
-	public class Dinglemouse 
-	{
+    public class Dinglemouse
+    {
+        public static int[] humanYearsCatYearsDogYears(int humanYears) //humanYears >= 1
+        {
+            // Your code here!
+            var catYears = 0;
+            var dogYears = 0;
+            for (int i = 1; i <= humanYears; i++)
+            {
+                if (i == 1)
+                {
+                    catYears += 15;
+                    dogYears += 15;
+                }
+                else if (i == 2)
+                {
+                    catYears += 9;
+                    dogYears += 9;
+                }
+                else if (i > 2)
+                {
+                    catYears += 4;
+                    dogYears += 5;
+                }
+            }
+            return new int[] { humanYears, catYears, dogYears };
+        }
+        public static int[] humanYearsCatYearsDogMonths(int humanMonths)
+        {
+            var catMonths = 0;
+            var dogMonths = 0;
+            var catMonthsFirstYear = 0;
+            var dogMonthsFirstYear = 0;
+            var catMonthsSecondYear = 0;
+            var dogMonthsSecondYear = 0;
+            var catMonthsThirdYear = 0;
+            var dogMonthsThirdYear = 0;
 
-		public static int[] humanYearsCatYearsDogYears(int humanYears) {
+            for (int i = 1; i <= humanMonths; i++)
+            {
+                if (i <= 23)// inseamna 1 an si 11 luni
+                {
+                    catMonthsFirstYear += 15;
 
-			int catYears = 0;
-			int dogYears = 0;
+                    dogMonthsFirstYear += 15;
+                }
+                if (i < 35 && i > 23)// inseamna anul 2 si 11 luni
+                {
+                    catMonthsSecondYear += 9;
+                    dogMonthsSecondYear += 9;
+                }
+                if (i >= 36) // peste sau egal cu 3 ani
+                {
+                    catMonthsThirdYear += 4;
+                    dogMonthsThirdYear += 5;
+                }
+            }
+            catMonths = catMonthsFirstYear + catMonthsSecondYear + catMonthsThirdYear; //toate luniile pisicesti 
+            dogMonths = dogMonthsFirstYear + dogMonthsSecondYear + dogMonthsThirdYear; //toate luniile cainesti
 
-			if (humanYears == 1)
-			{
-				catYears += 15;
-				dogYears += 15;
-			}
-			else if (humanYears == 2)
-			{
-				catYears += 24;
-				dogYears += 24;
-			}
-			else if (humanYears >= 3)
-			{
-				catYears = 24 + (humanYears - 2) * 4;
-				dogYears = 24 + (humanYears - 2) * 5;
-			}
-
-			return new int[] { humanYears, catYears, dogYears };
-		}
-	}
-
-	public class DinglemouseRecursive 
-	{
-		public int humanYears;
-
-		public static int CatYears(int humanYears)
-		{
-			int catYears = 0;
-
-			if (humanYears == 1)
-			{
-				return 15;
-			}
-			else if (humanYears == 2)
-			{
-				return 9 + CatYears(humanYears - 1); 
-			}
-			else
-			{
-				return 4 + CatYears(humanYears - 1);
-			}
-		}
-
-		public static int DogYears(int humanYears)
-		{
-			int dogYears = 0;
-
-			if (humanYears == 1)
-			{
-				return 15;
-			}
-			else if (humanYears == 2)
-			{
-				return 9 + DogYears(humanYears - 1); 
-			}
-			else
-			{
-				return 5 + DogYears(humanYears - 1);
-			}
-		}
-
-		public static int[] humanYearsCatYearsDogYears(int humanYears) 
-		{
-			return new int[] { humanYears, CatYears, DogYears };
-		}
-	}
+            return new int[] { humanMonths, catMonths, dogMonths };
+        }
+    }
 }
-
+/*
+Cat Years
+15 cat years for first year
++9 cat years for second year
++4 cat years for each year after that
+Dog Years
+15 dog years for first year
++9 dog years for second year
++5 dog years for each year after that*/
