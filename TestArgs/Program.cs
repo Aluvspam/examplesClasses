4aa099c93d25e19a74e9074de020a05008b34470
andrei patrascu <andreipatrascu@outlook.com>
2018-03-10 12:33:24 +0200

    added TestArgs project

---
+++ TestArgs/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestArgs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length > 0)
+            {
+                Console.WriteLine("hello {0}", args[0]);
+            }
+            else
+            {
+                Console.WriteLine("no args received");
+            }
+            Console.ReadLine();
+        }
+    }
+}
