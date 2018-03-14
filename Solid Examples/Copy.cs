2ecd628d6e19f471cb74ff3ddc9d22236c447bfe
andrei patrascu <andreipatrascu@outlook.com>
2018-03-14 20:28:04 +0200

    example dependencies

---
+++ Solid Examples/Copy.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+{
+    public class Copy
+    {
+        public void CopyIt()
+        {
+            var text = new ReadKeyboard().ReadIt();
+            new WriteScreen().WriteIt(text);
+        }
+    }
+}
---
+++ Solid Examples/WriteScreen.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+{
+    public class WriteScreen
+    {
+        public void WriteIt(string text)
+        {
+            Console.WriteLine(text);
+        }
+    }
+}
