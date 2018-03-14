09bd1013c5873ea6423297372790b817f34335bb
Cristian <cristian_radu_89@yahoo.com>
2018-03-14 19:36:32 +0200

    metoda square ( ciordita :) )

---
+++ Solid Examples/SquareCristi.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+{
+    class SquareCristi
+    {
+        static void Main(string[] args)
+
+        {
+            int n = int.Parse(Console.ReadLine());
+            Console.WriteLine(new string('*', n));
+            for (int i = 0; i < n - 2; i++)
+            {
+                Console.WriteLine("*" + new string(' ', n - 2) + "*");
+            }
+            Console.WriteLine(new string('*', n));
+            Console.ReadLine();
+        }
+    }
+}
+
