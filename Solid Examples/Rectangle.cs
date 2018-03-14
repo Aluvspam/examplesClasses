303c0ff553e400cf47cc5aa3802bc7402f4442ef
andrei patrascu <andreipatrascu@outlook.com>
2018-03-14 19:13:36 +0200

    added stuff like clash, rectangle etc

---
+++ Solid Examples/Rectangle.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+{
+    public class Rectangle
+    {
+        private double width;
+        private double height;
+        public double Width
+        {
+            get
+            {
+                return width;
+            }
+            set
+            {
+                width = value;
+            }
+        }
+        public double Height
+        {
+            get
+            {
+                return height;
+            }
+            set
+            {
+                height = value;
+            }
+        }
+    }
+}
