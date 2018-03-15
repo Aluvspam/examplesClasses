61857e37927dc5dbe51af49401bf82f27e2e25bf
oana.buturas <oana.buturas@gmail.com>
2018-03-15 14:23:02 +0200

    added ShortestWord class

---
+++ ExercisesCsharp/ShortestWord.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace ExercisesCsharp
+
+    //Simple, given a string of words, return the length of the shortest word(s).
+    //String will never be empty and you do not need to account for different data types.
+
+    {
+        public class ShortestWord
+        {
+            public static int FindShort(string s)
+            {
+                string[] words = s.Split(" ");
+                int shortestWord = Int32.MaxValue;
+
+                foreach (String w in words)
+                {
+                    if (w.Length < shortestWord)
+                    {
+                        shortestWord = w.Length;
+                    }
+                }
+
+                return shortestWord;
+            }          
+        }
+    }
