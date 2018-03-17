e2bf3f94382c5d49574dfbbb4454a8a7dc1f392e
Dana <danaciocoiu78@gmail.com>
2018-03-17 15:32:15 +0200

    WriteData

---
+++ Solid Examples/WriteDana.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+{
+    class WriteDana
+    {
+
+        public void WriteIt(string text)
+        {
+
+            for (int i = 1; i <= 7; i++)
+            {
+                for (int j = 1; j <= i; j++)
+                {
+                    Console.ForegroundColor = ConsoleColor.DarkYellow;
+                    Console.Write(" {0} {1} {2} ", '\u263c', text.ToUpper(), '\u263A');
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
---
+++ Solid Examples/WriteDanaWinforms.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace Solid_Examples
+{
+    partial class WriteDanaWinforms
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lblTriangle = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lblTriangle
+            // 
+            this.lblTriangle.Font = new System.Drawing.Font("Arial", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTriangle.Location = new System.Drawing.Point(13, 13);
+            this.lblTriangle.Name = "lblTriangle";
+            this.lblTriangle.Size = new System.Drawing.Size(613, 479);
+            this.lblTriangle.TabIndex = 0;
+            this.lblTriangle.Text = "Start";
+            this.lblTriangle.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.lblTriangle.Click += new System.EventHandler(this.lblTriangle_Click);
+            // 
+            // WriteDanaWinforms
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(638, 489);
+            this.Controls.Add(this.lblTriangle);
+            this.Name = "WriteDanaWinforms";
+            this.Text = "WriteDanaWinforms";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblTriangle;
+    }
+}
