c4e10a151de5f19aa36a9689ea98888d0a40f05f
daniel.ivanica <daniel.ivanica@gmail.com>
2018-03-17 20:08:39 +0200

    WriteDaniel - Changes

---
+++ Solid Examples/WriteDaniel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+{
+    class WriteDaniel
+    {
+        public string WriteIt(string text)
+        {
+            char[] cArray = text.ToCharArray();
+            string Reverse = String.Empty;
+            for (int i = cArray.Length - 1; i < -1; i--)
+            {
+                Reverse += cArray[i];
+            }
+            return Reverse;
+        }
+    }
+}
