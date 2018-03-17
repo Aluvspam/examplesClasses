2ebaf1f6796b7245a87f17e0c2729128387bec7b
Cristian <cristian_radu_89@yahoo.com>
2018-03-17 17:06:27 +0200

    am adaugat writeCristi cu modificarea textului doar after text si before text.

---
+++ Solid Examples/WriteCristi.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+{
+    class WriteCristi
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                string before = "-=";
+                string after = "=-";
+                Console.WriteLine("Please enter yout text here...");
+                string text = Console.ReadLine();
+                Console.Write(before + " " + text + " " + after);
+                Console.ReadLine();
+            }
+        }
+    }
+}
