a16d855a6413a68f80a610af129a0d500a15b4c8
oana.buturas <oana.buturas@gmail.com>
2018-03-18 10:48:09 +0200

    InitCrew() changes

--- ExampleInheritance/Game/AbstractUnit.cs
+++ ExampleInheritance/Game/AbstractUnit.cs
@@ -9,15 +9,18 @@ namespace Solid_Examples.Game
         public int Damage;
         public int RateOfFire;//time between 2 consecutive hits in miliseconds
         public Point[] Location;
-        public Person[] Crew;
+        public List<Person> crew;
 
-        public AbstractUnit(Point[] location, int hp, int dmg, int crewSize)
+        public AbstractUnit(Point[] location, int hp, int dmg)
         {
             Location = location;
             HitPoint = hp;
             Damage = dmg;
-            Crew = new Person[crewSize];
+            crew = new List<Person>();
         }
-        public abstract void InitCrew();
+        public abstract void InitCrew()
+        {
+            crew.Add(new Person());
+        }
     }
 }
