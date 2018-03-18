2ad069feded438c90c9bd08980affd5ec1d6704f
Andrei <Aluvspam@yahoo.com>
2018-03-18 09:55:26 +0200

    added helicopter

---
+++ ExampleInheritance/Game/Helicopter.cs
@@ -0,0 +1,22 @@
+﻿using ClassLibrary1;
+using System;
+
+namespace Solid_Examples.Game
+{
+    public class Helicopter : AbstractUnit
+    {
+        public int altitude;
+        public Helicopter(Point[] location) : base(location, 100, 35, 4)
+        {
+            altitude = 0;
+        }
+
+        public override void InitCrew()
+        {
+            Crew[0] = (new Person("Captain", DateTime.Now, null));
+            Crew[1] = (new Person("Gunner", DateTime.Now, null));
+            Crew[2] = (new Person("Gunner2", DateTime.Now, null));
+            Crew[3] = (new Person("1stOfficer", DateTime.Now, null));
+        }
+    }
+}
