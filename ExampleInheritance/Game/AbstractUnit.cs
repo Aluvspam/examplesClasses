661b955b3692c7e31b000a07c945a29c88d216dc
daniel.ivanica <daniel.ivanica@gmail.com>
2018-03-18 11:05:47 +0200

    modificare AU

--- ExampleInheritance/Game/AbstractUnit.cs
+++ ExampleInheritance/Game/AbstractUnit.cs
@@ -11,12 +11,12 @@ namespace Solid_Examples.Game
         public Point[] Location;
         public Person[] Crew;
 
-        public AbstractUnit(Point[] location, int hp, int dmg, int crewSize)
+        public AbstractUnit(Point[] location, int hp, int dmg, int crewSize = -1)
         {
             Location = location;
             HitPoint = hp;
             Damage = dmg;
-            Crew = list<Person>;
+            Crew = list<Person>();
             Crew.Add(new Person(new Point[1] {new Point (0, 9)} ));
         }
         public abstract void InitCrew();
