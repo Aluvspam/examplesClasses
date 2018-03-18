5bf19e5356098ba2205463905c90597672c50098
Dana <danaciocoiu78@gmail.com>
2018-03-18 10:54:11 +0200

    test Dana

--- Solid Examples/Game/AbstractUnit.cs
+++ Solid Examples/Game/AbstractUnit.cs
@@ -1,5 +1,6 @@
 ﻿//varianta B
 using ClassLibrary1;
+using System.Collections.Generic;
 
 namespace Solid_Examples.Game
 {
@@ -9,8 +10,9 @@ namespace Solid_Examples.Game
         public int Damage;
         public int RateOfFire;//time between 2 consecutive hits in miliseconds
         public Point[] Location;
-        private Person[] crew;
+        private List <Person> Crew;
         private int crewIndex;
+
 
         public AbstractUnit(Point[] location, int hp, int dmg, int crewSize)
         {
@@ -18,11 +20,16 @@ namespace Solid_Examples.Game
             HitPoint = hp;
             Damage = dmg;
             crewIndex = 0;
-            crew = new Person[crewSize];
+            Crew = new List<Person>();
+            //int crew size = -1;
         }
         public abstract void InitCrew();
         protected void AddCrewMember(Person p)
         {
+            List<Person> crew = new List<Person>();
+            Crew.Add(Person p);
+            
+
             crew[crewIndex] = p;
             crewIndex++;
         }
