446fcf15b831166330ce4ff17b2b02ede6676af3
Andrei <Aluvspam@yahoo.com>
2018-03-18 12:48:27 +0200

    fix WriteDaniel

--- Solid Examples/WriteDaniel.cs
+++ Solid Examples/WriteDaniel.cs
@@ -12,7 +12,7 @@ namespace Solid_Examples
         {
             char[] cArray = text.ToCharArray();
             string Reverse = String.Empty;
-            for (int i = cArray.Length - 1; i < -1; i--)
+            for (int i = cArray.Length - 1; i > -1; i--)
             {
                 Reverse += cArray[i];
             }
