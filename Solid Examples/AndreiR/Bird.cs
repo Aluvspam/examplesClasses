30632bcd47f873d6be0a1d1942ee8213ce0b7f06
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-03-21 19:40:14 +0200

    AndreiR -folder

---
+++ Solid Examples/AndreiR/Bird.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.AndreiR
+{
+   public class Bird: IMove
+    {
+        string name = "Bird";
+        public void Move()
+        {
+            Console.WriteLine("I am a {0} and I fly", name);
+        }
+    }
+}
---
+++ Solid Examples/AndreiR/Dog.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.AndreiR
+{
+    public class Dog : IMove
+    {
+        public void Move()
+        {
+            Console.WriteLine("walk on 4 legs");
+        }
+    }
+}
