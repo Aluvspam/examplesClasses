c9b145d660e757560d2889984e26604bc95fb6d8
andrei patrascu <andreipatrascu@outlook.com>
2018-03-21 21:07:33 +0200

    added Decorator project

---
+++ Solid Examples/Decorator/Decorator/Run.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Decorator
+{
+    public class Run
+    {
+        public static void Execute()
+        {
+            // Let us create a Simple Cake Base first
+            CakeBase cBase = new CakeBase();
+            PrintProductDetails(cBase);
+
+            // Lets add cream to the cake
+            CreamDecorator creamCake = new CreamDecorator(cBase);
+            PrintProductDetails(creamCake);
+
+            // Let now add a Cherry on it
+            CherryDecorator cherryCake = new CherryDecorator(creamCake);
+            PrintProductDetails(cherryCake);
+
+            // Lets now add Scent to it
+            ArtificialScentDecorator scentedCake = new ArtificialScentDecorator(cherryCake);
+            PrintProductDetails(scentedCake);
+
+            // Finally add a Name card on the cake
+            NameCardDecorator nameCardOnCake = new NameCardDecorator(scentedCake);
+            PrintProductDetails(nameCardOnCake);
+
+            // Lets now create a simple Pastry
+            PastryBase pastry = new PastryBase();
+            PrintProductDetails(pastry);
+
+            // Lets just add cream and cherry only on the pastry 
+            CreamDecorator creamPastry = new CreamDecorator(pastry);
+            CherryDecorator cherryPastry = new CherryDecorator(creamPastry);
+            PrintProductDetails(cherryPastry);
+        }
+
+        private static void PrintProductDetails(BakeryComponent cBase)
+        {
+            Console.WriteLine("Item: {0} Price: {1}", cBase.GetName(), cBase.GetPrice()); ;
+        }
+    }
+}
