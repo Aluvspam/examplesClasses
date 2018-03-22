ad9678b358a1997decfcc9644e5e1d168252994b
Dana <danaciocoiu78@gmail.com>
2018-03-22 14:01:05 +0200

    Strategy

---
+++ StrategyDana/Bird.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDana
+{
+    class Bird
+    {
+         string colour;
+        string wait;
+        public Move()
+        {
+            Console.WriteLine();
+        }
+    }
+}
---
+++ StrategyDana/World.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrategyDana
+{
+    class World
+    {
+        string colour;
+        string sex;
+
+        IMove myPet;
+        public World(IMove Something)
+        {
+            Dog = dog;
+            Cat = cat;
+
+        }
+        public void execute()
+            {
+            myPet.Move;
+            }
+        
+    }
+}
