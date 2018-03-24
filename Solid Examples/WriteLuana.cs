de5bb80d15a50cbe6aa36e9f3222af5acafa29d3
KalamityLu <luana.barbulescu@gmail.com>
2018-03-24 10:48:59 +0200

    WriteLuana

---
+++ Solid Examples/WriteLuana.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+{
+	class WriteLuana
+	{
+		public void WriteIt(string text)
+		{
+			text = System.Threading.Thread.CurrentThread.CurrentCulture.TextInfo.ToTitleCase(text);
+			Console.WriteLine(text);
+		}
+	}
+}
