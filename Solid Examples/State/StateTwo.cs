9dd194b05ad8b8316b403dc3799195560e41da0d
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-03-26 20:28:49 +0300

    Cristi Math in StateTow class

--- Solid Examples/State/StateTwo.cs
+++ Solid Examples/State/StateTwo.cs
@@ -13,5 +13,27 @@ namespace Solid_Examples.State
         {
             throw new NotImplementedException();
         }
+        public static int CristiMath(bool b, int x)
+        {
+            double a = Convert.ToDouble(x);
+            double result = 0.0;
+            if( b == true && x >= 0)
+            {
+                result= Math.Pow(a, 2.00);
+            }
+            else if( b == true && x <0)
+            {
+                result = Math.Sqrt(-a);
+            }
+            else if (b == false && x >=0)
+            {
+                result = Math.Sqrt(a);
+            }
+            else if (b == false && x <0)
+            {
+                result = Math.Sqrt(-a);
+            }
+            return Convert.ToInt32(result);
+        }
     }
 }
