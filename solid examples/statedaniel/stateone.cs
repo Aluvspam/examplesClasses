4761b832ec94a17f8e567ea5cdec3524b9cb12ed
daniel.ivanica <daniel.ivanica@gmail.com>
2018-03-26 20:46:57 +0300

    Completare metoda

---
+++ solid examples/statedaniel/stateone.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.StateDaniel
+{
+    class StateOne : State
+    {
+        public void goNext(Context context)
+        {
+            context.setState(new StateTwo());
+        }
+        public double DanielMath(bool d, int x)
+        {
+            double result = 0;
+
+            if (d == true && x >= 0)
+            {
+                result = x * x;
+            }
+            else if (d == false && x >= 0)
+            {
+                result = Math.Sqrt(x);
+            }
+            else if (d == true && x <= 0)
+            {
+                result = -(x * x);
+            }
+            else if (d == false && x <= 0)
+            {
+                result = -(Math.Sqrt(x));
+            }
+            return result;
+        }
+    }
+}
