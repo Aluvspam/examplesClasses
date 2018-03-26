cd692f0d38d704b56c742a8bc1bb80574bdc5dc2
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-03-26 20:48:18 +0300

    CristiMathTests()

---
+++ Solid ExamplesTests/State/StateTwoTests.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Solid_Examples.State;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.State.Tests
+{
+    [TestClass()]
+    public class StateTwoTests
+    {
+        [TestMethod()]
+        public void CristiMathTest()
+        {
+            Assert.AreEqual(StateTwo.CristiMath(true, 2), 4);
+        }
+        [TestMethod()]
+        public void CristiMathTest2()
+        {
+            Assert.AreEqual(StateTwo.CristiMath(true, -1), 1);
+        }
+        [TestMethod()]
+        public void CristiMathTest3()
+        {
+            Assert.AreEqual(StateTwo.CristiMath(false, 2), 4);
+        }
+        [TestMethod()]
+        public void CristiMathTest4()
+        {
+            Assert.AreEqual(StateTwo.CristiMath(false, -1), 1);
+        }
+    }
+}
