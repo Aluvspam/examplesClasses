0485ac8ca40e8d78a596e736c1856f62ccd8d3a4
CorlyBoy <gabicorlan82@gmail.com>
2018-03-26 20:33:17 +0300

    Corly's Method

---
+++ StateProject/StateTwo.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateProject
+{
+    class StateTwo : State
+    {
+        public override void goNext(Context inContext)
+        {
+            throw new NotImplementedException();
+        }
+
+        public double GabiMath(bool b, int x)
+        {
+            double result = 0;
+
+            if (b = true && x >= 0)
+            {
+                result = x * x;
+            }
+            else if (b = false && x >= 0)
+            {
+                result = Math.Sqrt(x);
+            }
+            else if (b = true && x < 0)
+            {
+                result = -(x * x);
+            }
+            else if (b = false && x < 0)
+            {
+                result = -Math.Sqrt(-x);
+            }
+
+            return result;
+        }
+    }
+}
