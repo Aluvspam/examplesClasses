b2ced6c8a89ef96b70ad327656178bf922fe53d0
daniel.ivanica <daniel.ivanica@gmail.com>
2018-03-26 20:06:52 +0300

    StateDaniel

---
+++ Solid Examples/StateDaniel/Context.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.StateDaniel
+{
+    class Context
+    {
+        private void current()
+        {
+
+        }
+        public override goNext()
+        {
+            current.goNext(this);
+        }
+        public void setState()
+        {
+
+        }
+    }
+}
