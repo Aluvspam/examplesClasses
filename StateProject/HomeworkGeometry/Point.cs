1582a0be771775877d38983cc806c28570ba97ac
CorlyBoy <gabicorlan82@gmail.com>
2018-03-27 11:16:13 +0300

    Geometry Project

---
+++ StateProject/HomeworkGeometry/Point.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateProject.HomeworkGeometry
+{
+    class Point
+    {
+        #region Fields
+        private short x;
+        private short y;
+        #endregion
+
+        #region Properties
+        public short X { get; set; }
+        public short Y { get; set; }
+        #endregion
+
+        #region Constructors
+        public Point(short x, short y)
+        {
+
+        } 
+        #endregion
+    }
+}
---
+++ StateProject/HomeworkGeometry/Triangle.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StateProject.HomeworkGeometry
+{
+    class Triangle
+    {
+        #region Fields
+        private Point[] points;
+        #endregion
+
+        #region Properties
+        public Point[] Points { get; set; }
+        public Segment[] Edges { get; set; }
+        public double Area { get; set; }
+        #endregion
+
+        #region Constructors
+        public Triangle(Point k, Point l, Point m)
+        {
+
+        }
+
+        public Triangle(int[] array)
+        {
+
+        } 
+        #endregion
+    }
+}
