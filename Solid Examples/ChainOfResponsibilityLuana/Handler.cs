130de3e503a2693cfc032878af3ceb234925c98d
KalamityLu <luana.barbulescu@gmail.com>
2018-03-27 17:35:47 +0300

    COR_Luana

---
+++ Solid Examples/ChainOfResponsibilityLuana/Handler.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.ChainOfResponsibilityLuana
+{
+	public abstract class Handler
+	{
+		protected Handler super;
+		protected Handler nextHandler;
+
+		public abstract void HandleRequest();
+	} 
+	
+}
---
+++ Solid Examples/ChainOfResponsibilityLuana/HandlerOne.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.ChainOfResponsibilityLuana
+{
+	public class HandlerOne : Handler
+	{
+		public override void HandleRequest()
+		{
+			if(CanHandle())
+			{
+				this.CanHandle();
+			}
+			else
+			{
+				super.HandleRequest();
+			}
+		}
+
+		public bool CanHandle()
+		{
+			return true;
+		}
+	}
+}
