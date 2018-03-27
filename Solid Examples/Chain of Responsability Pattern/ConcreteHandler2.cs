350e371e3faca9d5fcc53edc7c41ef7b5754a818
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-03-27 23:12:28 +0300

    Added Chain of Responsability Pattern

---
+++ Solid Examples/Chain of Responsability Pattern/ConcreteHandler2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.Chain_of_Responsability_Pattern
+{
+    public class ConcreteHandler2 : Handler
+    {
+        public override void HandleRequest()
+        {
+
+        }
+    }
+}
---
+++ Solid Examples/Chain of Responsability Pattern/Handler.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.Chain_of_Responsability_Pattern
+{
+    public abstract class Handler
+    {
+        public abstract void HandleRequest();
+    }
+}
