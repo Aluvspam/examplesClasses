24964ab13b87cca8c199d3461a2f82c41ffcfecf
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-03-28 19:13:17 +0300

    endpoints update

--- Geometry/Segment.cs
+++ Geometry/Segment.cs
@@ -14,16 +14,17 @@ namespace Geometry
             get { return endpoints; }
             set { endpoints = value; }
         }
-        public double Length//a^2 + b^2 = c^2 - teorema lui Pitagora
-        {
-            set
-            {
-                  //value = Math.Pow((EndPoints[0]- EndPoints[2]), 2.0) + Math.Pow((EndPoints[1]- EndPoints[3]), 2.0);
-            }
-        }
+       
         public Segment(short x1, short y1, short x2, short y2)
         {
-            Point[] segment = new Point[] { new Point(x1, y1), new Point(x2, y2) };
+            endpoints = new Point[] { new Point(x1, y1), new Point(x2, y2) };
+        }
+        public double Length//a^2 + b^2 = c^2 - teorema lui Pitagora
+        {
+            get
+            {
+                return Math.Pow((endpoints[0].X - endpoints[1].X), 2.0) + Math.Pow((endpoints[0].Y - endpoints[1].Y), 2.0);
+            }
         }
     }
 }
