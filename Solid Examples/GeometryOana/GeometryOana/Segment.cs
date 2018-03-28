4e057a678d366e047a730ef7f3f500dbe61b230d
oana.buturas <oana.buturas@gmail.com>
2018-03-28 15:23:27 +0300

    Geometry proj

---
+++ Solid Examples/GeometryOana/GeometryOana/Segment.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryOana
+{
+    class Segment
+    {
+        private Point[] endpoints;
+
+        public Point[] Endpoints;
+        public double Length;
+
+        public Segment(short a, short b, short c, short d)
+        {
+
+        }
+    }
+}
---
+++ Solid Examples/GeometryOana/GeometryOana/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GeometryOana
+{
+    class Triangle
+    {
+        private Point[] points;
+
+        public Point[] Points;
+        public Segment[] Edges;
+        public double Area;
+
+        public Triangle(Point p1, Point p2, Point p3)
+        {
+            
+        }
+
+        public Triangle(Point[] p) 
+        {
+
+        }
+    }
+}
