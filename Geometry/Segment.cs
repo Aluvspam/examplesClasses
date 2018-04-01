aa72c418d6325b782ded4704dc04562427d64c38
KalamityLu <luana.barbulescu@gmail.com>
2018-04-01 15:18:01 +0300

    Geometry

---
+++ Geometry/Segment.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Geometry
+{
+	class Segment
+	{
+		private Point[] endpoints;
+		public Point[] Endpoints { get; set; }
+		public double Length { get; set; }
+
+		public Segment(short a, short b, short c, short d)
+		{
+
+		}
+	}
+}
