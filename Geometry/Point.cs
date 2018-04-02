4f90fbc6a52f20d6be483d5f566b262dc312541e
KalamityLu <luana.barbulescu@gmail.com>
2018-04-02 20:31:37 +0300

    Cosmetizare #regions

--- Geometry/Point.cs
+++ Geometry/Point.cs
@@ -8,23 +8,30 @@ namespace Geometry
 {
 	public class Point
 	{
+		#region Fields
 		private short x;
 		private short y;
+		#endregion
+		#region Properties
 		public short X
 		{
 			get { return x; }
 			set { x = value; }
 		}
+		
 		public short Y
 		{
 			get { return y; }
 			set { y = value; }
 		}
+		#endregion
+		#region Ctor
 		public Point(short x, short y)
 		{
 			this.x = x;
 			this.y = y;
 		}
+		#endregion
 	}
 
 }
