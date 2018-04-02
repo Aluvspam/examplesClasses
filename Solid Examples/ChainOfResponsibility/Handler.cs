ca789b1744b7a147ce5c6324a4a4a8add1990228
daniel.ivanica <daniel.ivanica@gmail.com>
2018-04-02 14:10:12 +0300

    Created ChainOfResponsiblity Deleted folder Geometry

---
+++ Solid Examples/ChainOfResponsibility/Handler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.ChainOfResponsibility
+{
+    public abstract class Handler
+    {
+        protected Handler super;
+        private Handler nextHandler;
+
+        public virtual void Handle()
+        {
+            nextHandler.Handle();
+        }
+    }
+}
---
+++ Solid Examples/ChainOfResponsibility/HandlerTwo.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.ChainOfResponsibility
+{
+    class HandlerTwo : Handler
+    {
+        public override void Handle()
+        {
+            if (CanHandle())
+            {
+                this.CanHandle();
+            }
+            else
+            {
+                super.Handle();
+            }
+        }
+        private bool CanHandle()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
+
+
