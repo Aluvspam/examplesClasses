1508ff945d98cad7d658f2916ff7d89e8784127c
CorlyBoy <gabicorlan82@gmail.com>
2018-04-02 19:45:20 +0300

    Length property in Segment class

--- GeometryGabi/Segment.cs
+++ GeometryGabi/Segment.cs
@@ -17,14 +17,18 @@ namespace GeometryGabi
             get { return endpoints; }
             set { endpoints = value; }
         }
-        public double Length { get; set; }
+        public double Length {
+            get {
+                return Math.Sqrt(Math.Pow((endpoints[0].X - endpoints[1].X), 2) + Math.Pow((endpoints[0].Y - endpoints[1].Y), 2);
+                    }
+            set { };
+        }
         #endregion
 
         #region Constructors
         public Segment(short x1, short y1, short x2, short y2)
         {
             endpoints = new Point[2] {new Point(x1, y1), new Point(x2, y2) };
-            
         } 
         #endregion
     }
