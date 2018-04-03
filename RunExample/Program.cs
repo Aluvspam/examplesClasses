991b9a180399054f6311e5090b7e9f282a1b6840
Andrei <Aluvspam@yahoo.com>
2018-04-03 19:29:17 +0300

    we run events and delegates from RunExample. people need to call their method that abonates them to the event here

--- RunExample/Program.cs
+++ RunExample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using ClassLibrary1;
 using Solid_Examples;
 using Solid_Examples.AndreiR;
+using EventsAndDelegates;
 
 namespace RunExample
 {
@@ -10,6 +11,8 @@ namespace RunExample
     {
         static void Main(string[] args)
         {
+            RunEventsAndDelegates();
+            return;//we know code above this is no longer executed, we can comment or delete this if we want to try that old example
             Decorator.Run.Execute();
             var world = new World();
             world.execute();
@@ -74,6 +77,12 @@ namespace RunExample
             int a4 = ((Teacher)AndyDeLa).Wage + 1;
             Console.ReadLine();
         }
+
+        private static void RunEventsAndDelegates()
+        {
+            ExampleEvent.Main(new string[0]);
+        }
+
         private static Person Promote(Person pupil)
         {
             return new Teacher(pupil.Name, pupil.DOB, pupil.Address, 0);
