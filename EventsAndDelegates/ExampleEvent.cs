5ac32368578275d0878aa6430444c409445d6c84
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-04-05 13:06:19 +0300

    commit local

--- EventsAndDelegates/ExampleEvent.cs
+++ EventsAndDelegates/ExampleEvent.cs
@@ -30,21 +30,23 @@ namespace EventsAndDelegates
         public event EventHandler Xevent3;
         public static void Main(string[] args)
         {
+            AndreisArgs objAnd = new AndreisArgs(4.67);
+            EventArgs objEv = new EventArgs();
             var r = new Random();
             int i = 0;
             while (true)
             {
-                if (i == r.Next(100))
+                if (i == r.Next(1000))
                 {
                     Xevent.Invoke(r.Next(10) + 1);
                 }
                 else if (i / 2 == r.Next(1000))
                 {
-                   Xevent2.Invoke(new ExampleEvent(), new EventArgs());
+                    Xevent2.Invoke(new ExampleEvent(), objEv);
                 }
                 else
                 {
-                  Xevent2.Invoke(new ExampleEvent(), new AndreisArgs(4.67));
+                    Xevent2.Invoke(new ExampleEvent(), objAnd);
                 }
                 i++;
             }
