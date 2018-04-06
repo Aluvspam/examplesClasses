3a797531f556560842dc47588cdd26f550dbf5c3
Andrei <Aluvspam@yahoo.com>
2018-04-06 10:55:26 +0300

    example Queue

--- Solid Examples/Strategy/World.cs
+++ Solid Examples/Strategy/World.cs
@@ -3,16 +3,26 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using EventsAndDelegates;
 
 namespace Solid_Examples.AndreiR
 {
     public class World
     {
+        Queue<AbstractEntity> coada;
+        public World()
+        {
+            coada = new Queue<AbstractEntity>();
+            execute();
+        }
         public void execute()
         {
             var b = new Bird();
             var d = new Dog();
             var d2 = new Drone();
+            coada.Enqueue(b);
+            coada.Enqueue(d);
+            coada.Enqueue(d2);
             //object[] things= new object[3] { b, d, d2 };
             //foreach (var thing in things)
             //{
@@ -26,6 +36,37 @@ namespace Solid_Examples.AndreiR
             d.Move();
             d2.Move();
         }
-
+        public void OnXeventHandler(int x)
+        {
+            if (x % 5 == 0)
+            {
+                coada.Enqueue(new Bird());
+            }
+            else if (x % 5 == 1)
+            {
+                coada.Enqueue(new Dog());
+            }
+            else if (x % 5 == 2)
+            {
+                coada.Enqueue(new Drone());
+            }
+            else
+            {
+                var entity = coada.Dequeue();
+                entity.Move();
+            }
+        }
+        private void DequeOnAList()
+        {
+            var coditza = new List<AbstractEntity>();
+            coditza.Add(new Bird());
+            coditza.Add(new Dog());
+            coditza.Add(new Drone());
+            AbstractEntity first;
+            first = coditza[0];
+            coditza.Remove(coditza[0]);
+            AbstractEntity next = coditza[0];
+            coditza.Remove(coditza[0]);
+        }
     }
 }
