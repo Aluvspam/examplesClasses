ee7ef26bfbafbf5afe0aedbcc803d6dc16d06f08
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-04-11 20:19:02 +0300

    DoorState folder

---
+++ Solid Examples/StateDoor/Closing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.StateDoor
+{
+    class Closing: AbstractState
+    {
+        public override void goNext(Door door)
+        {
+
+        }
+    }
+}
---
+++ Solid Examples/StateDoor/Door.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples.StateDoor
+{
+    public class Door
+    {
+        AbstractState currentState;
+        AbstractState nextState;
+
+        public void goNext()
+        {
+            currentState.goNext(this);
+        }
+        public void SetState(AbstractState state)
+        {
+            currentState = state;
+        }
+        public void SetNext(AbstractState state)
+        {
+            if (state != null)
+            {
+                nextState = state; 
+            } 
+        }
+    }
+}
