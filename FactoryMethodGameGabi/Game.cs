05f40e483a4ec6a8dc3d21e2232d055080176978
CorlyBoy <gabicorlan82@gmail.com>
2018-04-16 20:09:09 +0300

    Factory Classes

---
+++ FactoryMethodGameGabi/Game.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodGameGabi
+{
+    public interface IGame
+    {
+        AbstractGameMap LoadEnvironment();
+
+        void PrintMapDetails();
+        
+
+        
+    }
+}
---
+++ FactoryMethodGameGabi/Level2.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FactoryMethodGameGabi
+{
+    class Level2 : IGame
+    {
+
+    }
+}
