7674173a3d3746747de3818e3c9db537336d668d
oana.buturas <oana.buturas@gmail.com>
2018-04-18 13:30:18 +0300

    added exercise Century from Year

---
+++ ExercisesCsharp/СenturyFromYear.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public static class Kata
+{
+    public static int СenturyFromYear(int year)
+    {
+        int rest = year % 100;
+        int century = year / 100;
+        if (rest >= 1)
+        {
+            return century + 1;
+        }
+        else
+        {
+            return century;
+        }
+    }
+}
