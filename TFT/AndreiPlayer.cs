ece973e125bafe6f18fc4a9d84a1fe50d8270dc9
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-04-22 11:13:49 +0300

    AndreiPlayer

---
+++ TFT/AndreiPlayer.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFT
+{
+    class AndreiPlayer : IPlayer
+    {
+        public Moves FirstMove()
+        {
+            return Moves.C;
+        }
+
+        public Moves MyMove(Moves myLastMove, Moves othersLastMove)
+        {
+            return MyStyle(myLastMove, othersLastMove);
+        }
+        private Moves MyStyle(Moves myMove, Moves enemyMove)
+        {
+            switch (enemyMove)
+            {
+                case Moves.B:
+                    myMove = Moves.C;
+                    break;
+                case Moves.C:
+                    myMove = Moves.D;
+                    break;
+                case Moves.D:
+                    myMove = Moves.D;
+                    break;
+                default:
+                    myMove = Moves.C;
+                    break;
+            }
+            return myMove;
+           
+           
+        }
+    }
+}
