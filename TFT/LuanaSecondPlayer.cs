97857435217a1e2e147b9ec6d1973bd959a832c2
KalamityLu <luana.barbulescu@gmail.com>
2018-04-23 18:25:12 +0300

    SecondPlayer

---
+++ TFT/LuanaSecondPlayer.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFT
+{
+    class LuanaSecondPlayer : IPlayer
+    {
+        public Moves FirstMove()
+        {
+            return Moves.D;
+        }
+
+        public Moves MyMove(Moves myLastMove, Moves othersLastMove)
+        {
+            if (myLastMove == Moves.C && othersLastMove == Moves.C)
+            {
+                return Moves.D;
+            }
+            else if (myLastMove == Moves.D && othersLastMove == Moves.D)
+            {
+
+                return Moves.C;
+            }
+            else if (myLastMove == Moves.C && othersLastMove == Moves.C)
+            {
+                return Moves.D;
+            }
+            else if (myLastMove == Moves.C && othersLastMove == Moves.D)
+            {
+                return Moves.D;
+            }
+            else
+            {
+                myLastMove = Moves.D;
+            }
+            return myLastMove;
+
+        }
+    }
+}
