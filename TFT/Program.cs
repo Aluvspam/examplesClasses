ef26052ac0a942ec12a7315bf40e0333176b771f
daniel.ivanica <daniel.ivanica@gmail.com>
2018-04-25 12:34:55 +0300

    DanielPlayer

--- TFT/Program.cs
+++ TFT/Program.cs
@@ -15,6 +15,7 @@ namespace TFT
 
         private static void RunTFT()
         {
+            IPlayer daniel = new DanielPlayer();
             IPlayer luana = new LuanaPlayer();
             IPlayer andrei = new AndreiPlayer();
             //IPlayer random = new RandomPlayer();
