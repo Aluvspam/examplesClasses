baa382b419f42b439eb12900c0c26c5e8b595f7c
andrei patrascu <andreipatrascu@outlook.com>
2018-05-02 18:15:51 +0300

    added player2.cs

---
+++ TFT/player2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TFT
+{
+    class Player2 : AbstractTFTPlayer
+    {
+        public override Moves FirstMove()
+        {
+            event2 += Event2Handler;
+            Invoke1();
+            strategy = new Atac();
+            return base.FirstMove();
+        }
+
+        public override void Event2Handler()
+        {
+            strategy = new Capra();
+        }
+
+        public Player2()
+        {
+            strategy = new Capra();
+        }
+        public override Moves MyMove(Moves myLastMove, Moves othersLastMove)
+        {
+            return strategy.MoveLikeABoss(this);
+        }
+    }
+}
