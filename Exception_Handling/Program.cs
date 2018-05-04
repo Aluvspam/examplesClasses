eed0d67d130e2b087a7a69774f73c50e7d24688f
Dana <danaciocoiu78@gmail.com>
2018-05-04 15:24:02 +0300

    Excetion

---
+++ Exception_Handling/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exception_Handling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            label:
+            try // Try block: The code which may raise exception at runtime
+            {
+               
+
+                int num1, num2;
+                decimal result;
+                Console.WriteLine("Program impartire. Introduceti doua numere si returnam rezultatul: ");
+                Console.WriteLine("Primul numar: ");
+                num1 = int.Parse(Console.ReadLine());
+                Console.WriteLine("Introduceti al doilea numar:");
+                num2 = int.Parse(Console.ReadLine());
+                result = (decimal)num1 / (decimal)num2;
+                Console.WriteLine("Impartirea : " + result.ToString());
+                Console.ReadLine();
+
+                //------------------------------------------------
+
+            }
+            catch (DivideByZeroException dex) //The 'catch' block handle the exception if try block gets error 
+            {
+
+                Console.WriteLine("Ai introdus 0 0");
+                Console.WriteLine("Mai multe detalii despre eroare: \n\n" + dex.ToString() + "\n\n");
+                goto label;
+
+            }
+            catch       (FormatException fex)
+            {
+                Console.WriteLine("Intrare invalida");
+                Console.WriteLine("Mai multe detalii despre eroare: \n\n" + fex.ToString() + "\n\n");
+                goto label;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Othe Exception raised" + ex.ToString() + "\n\n");
+                goto label;
+            }
+            finally //it always executes
+            {
+                Console.WriteLine("Finally Block: For Continue Press Enter and for Exit press Ctrl + c");
+                Console.ReadLine();
+            }
+
+        }
+    }
+}
---
+++ user_defined_exception/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_defined_exception
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int acceptorder;
+            Console.WriteLine("Welcome to Shopping Site:\nHow many TV you want to buy (Total 10 in Stock):");
+            acceptorder = Convert.ToInt32(Console.ReadLine());
+            try
+            {
+                if(acceptorder ==10 || acceptorder <10)
+                {
+                    Console.WriteLine("Congratulations! You have bought {0} TV", acceptorder);
+                    Console.ReadLine();
+                }
+                else
+                {
+                    throw (new OutofStockException("OutofStockException Generated: The number of item you want to buy is out of stock. Please enter total item number within stock"));
+                }
+            }
+            catch(OutofStockException oex)
+            {
+                Console.WriteLine(oex.Message.ToString());
+                Console.ReadLine();
+            }
+          
+        }
+        public class OutofStockException : Exception
+        {
+            public OutofStockException(string message)
+                :base(message)
+            {
+
+            }
+
+        }
+    }
+
+
+}
