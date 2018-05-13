793f0585527c487b362042cdb5f755aa99b1ee15
daniel.ivanica <daniel.ivanica@gmail.com>
2018-05-13 07:41:48 +0300

    Added Player2

--- TFT/DanielPlayer2.cs
+++ TFT/DanielPlayer2.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace TFT
 {
-    class DanielPlayer2
+    class DanielPlayer2 : IPlayer
     {
+        public Moves FirstMove()
+        {
+            return Moves.C;
+        }
+        public Moves MyMove(Moves myLastMove, Moves othersLastMove)
+        {
+            if (myLastMove == Moves.C && othersLastMove == Moves.C)
+            {
+                return Moves.C;
+            }
+            else
+            {
+                return Moves.D;
+            }
+        }
     }
 }
