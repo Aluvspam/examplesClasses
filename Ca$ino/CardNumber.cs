4e36e5d4da07d1dcc634c8ce673c1ecc25d8ed4d
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-05-17 20:17:59 +0300

    added sexines to Casino proj

---
+++ Ca$ino/CardNumber.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Casino
+{
+    public enum CardNumber
+    {
+        Two = 2,
+        Three = 3,
+        Four = 4,
+        Five = 5,
+        Six = 6,
+        Seven = 7,
+        Eight = 8,
+        Nine = 9,
+        Ten = 10,
+        J = 12,
+        Q = 13,
+        K = 14,
+        A = 15
+    }
+}
