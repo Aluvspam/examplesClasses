3bfea2f0192ec26dfc230b72732871fd473826b1
Dana <danaciocoiu78@gmail.com>
2018-05-17 19:23:56 +0300

    PullCard

--- Ca$ino/DeckOfCards.cs
+++ Ca$ino/DeckOfCards.cs
@@ -8,9 +8,29 @@ namespace Casino
 {
     public class DeckOfCards
     {
-        public static void PullCard()
-        {
+        public Card [] deck;
+        public Random ranNum;
+        public int currentCard;
+        public int numberOfCard = 52;
 
+        public DeckOfCards()
+        {
+            currentCard = 0;
+            ranNum = new Random();
+            deck = new Card;
+
+        }
+        public Card PullCard()
+        {
+            if (currentCard < deck.Length)
+            {
+                return deck[currentCard++];
+
+            }
+            else
+            {
+                return null;
+            }
         }
 
         public enum CardNumber
