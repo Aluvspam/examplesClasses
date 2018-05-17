c551d42de7df96c92f96ef656d24f50f4eead36c
daniel.ivanica <daniel.ivanica@gmail.com>
2018-05-17 18:07:23 +0300

    Created enum CardNo

--- Ca$ino/DeckOfCards.cs
+++ Ca$ino/DeckOfCards.cs
@@ -12,5 +12,22 @@ namespace Casino
         {
 
         }
+
+        public enum CardNumber
+        {
+            Two = 2,
+            Three = 3,
+            Four = 4,
+            Five = 5,
+            Six = 6,
+            Seven = 7,
+            Eight = 8,
+            Nine = 9,
+            Ten = 10,
+            J = 12,
+            Q = 13,
+            K = 14,
+            A = 15
+        }
     }
 }
