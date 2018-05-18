b4c8a27d2ce07e967d05c5c3cf99b9543d2e8867
KalamityLu <luana.barbulescu@gmail.com>
2018-05-18 08:13:14 +0300

    Dice_Class

---
+++ Ca$ino/Dice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Casino
+{
+    public class Dice
+    {
+        private int die1;
+        private int die2;
+        public int Die1
+        {
+            get { return die1; }
+            set { die1 = value; }
+        }
+        public int Die2
+        {
+            get { return die2; }
+            set { die2 = value; }
+        }
+        public void RollDice()
+        {
+            Random random1 = new Random();
+            Die1 = random1.Next(1, 6);
+            Die2 = random1.Next(1, 6);
+        }
+    }
+}
