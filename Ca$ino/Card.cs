1123409a142a0b70e66580ad1ab87e0460b5c143
oana.buturas <oana.buturas@gmail.com>
2018-05-18 13:45:14 +0300

    added (empty) override methods

--- Ca$ino/Card.cs
+++ Ca$ino/Card.cs
@@ -8,9 +8,43 @@ namespace Casino
 {
     public class Card
     {
-        public Card()
+        private CardNumber number;
+        //private CardColor color;  -> nu e inca implementata
+
+        public CardNumber number 
         {
-            
+            get 
+            { 
+                return number; 
+            }
+            set 
+            {
+                number = value;
+            }
+        }
+
+        /*
+        public CardColor color 
+        {
+            get 
+            { 
+                return color; 
+            }
+            set 
+            {
+                color = value;
+            }
+        }
+        */
+
+        public override bool Equals(object obj)
+        {
+            return base.Equals(obj);
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
         }
     }
 }
