9c165173d0b9efef2af0ebf1cbe731550b370a87
Andrei Roca <rocaandrei@users.noreply.github.com>
2018-06-02 13:42:30 +0300

    added implementation to BinaryTree - class Tree and class Node

---
+++ BinaryTree/Node.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Node
+    {
+        public int value;
+        public Node Left;
+        public Node Right;
+
+        public Node(int initial)
+        {
+            value = initial;
+            //cand creezi un obiect nou in copac, left si right nu sunt conectati deci sunt null
+            Left = null;
+            Right = null;
+        }
+    }
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Tree
+    {
+        Node top;//radacina
+
+        public Tree()//un copac fara nicio valoare
+        {
+            top = null;
+        }
+        public Tree(int initial)//un copac cu valoare initiala
+        {
+            top = new Node(initial);
+        }
+
+        public void Add(int value)
+        {
+            //non recursive
+        }
+        public void AddRC(int value)
+        {
+            AddR(ref top, value); //o sa ne zica exact unde trebuie sa adaugam valoarea 
+        }
+        public void AddR(ref Node N, int value)// si aici se intampla recursivitatea
+        {
+
+        }
+        public void Print(ref string newString)
+        {
+            //sa scriem copacul in ordine 
+        }
+    }
+}
