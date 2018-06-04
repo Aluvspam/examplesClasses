03ee13d259f78bfe1ceb41139f1fe96e31edde8f
Cristian <cristian_radu_89@yahoo.com>
2018-06-04 19:19:44 +0300

    added single dice

---
+++ Solid Examples/CristiSingleDice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Solid_Examples
+
+{
+    class CristiSingleDice
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {
+                Random rnddice = new Random();
+                int dice = rnddice.Next(1, 7);
+                Console.WriteLine(dice);
+                Console.ReadLine();
+            }
+        }
+    }
+}
